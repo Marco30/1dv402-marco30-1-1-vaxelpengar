ee6b4bc35124fcb963374c3d078e921a4d330137
marco30 <mv222fp@student.lnu.se>
2014-09-20 23:26:46 +0200

    'test'

---
+++ 1DV402.S1.1.L01A/1DV402.S1.1.L01A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S1._1.L01A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string M;
+            M = "hej";
+            Console.Write((double)(1 / 3));
+            Console.WriteLine(M);
+            int[] numbers = new int[2];
+            Console.WriteLine(numbers[1]);
+            M = Console.ReadLine();
+        }
+    }
+}
+
