583e87ac1c3dcf7fe4f99367329ace489af7bcd1
marco30 <mv222fp@student.lnu.se>
2014-09-21 02:02:24 +0200

    'lab'

--- 1DV402.S1.1.L01A/1DV402.S1.1.L01A/Program.cs
+++ 1DV402.S1.1.L01A/1DV402.S1.1.L01A/Program.cs
@@ -13,11 +13,11 @@ namespace _1DV402.S1._1.L01A
             double total;
             
             int betalat;
-            
-            while(true)
+
+            while (true) //loop startat och loppar förevigt om inte den avlutas med break; 
             {
 
-                try
+                try // öppnar up möjligheten att hantera fel chatch som kan uppstå i programmet  
                 {
 
 
@@ -25,20 +25,20 @@ namespace _1DV402.S1._1.L01A
                     Console.Write("Ange totalsumma:");
                     total = double.Parse(Console.ReadLine());
 
-                    if (total < 0.50)
+                    if (total < 0.50) // det här visas om beloppet man får är mindre än en krona 
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
                         Console.WriteLine("Suman är för liten, köp genomförs inte");
                         Console.ResetColor();
                     }
-                  
-                    else
+
+                    else // är det mer än en krona avlutat loppen med break  
                     {
                         break;
                     }
 
                 }
-                catch
+                catch // skriver man en bokstav, punkt eller något som inte uppfattas som ett tal så visas det här 
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("det är inte en siffra");
@@ -47,7 +47,38 @@ namespace _1DV402.S1._1.L01A
             
             }
 
+            int b = (int)Math.Round(total);
 
+            while (true)
+            {
+                try 
+                {
+                    Console.Write("mottaget belopp:");
+                    betalat = int.Parse(Console.ReadLine());
+                   
+                    if (betalat >= b)
+                    {
+                        break;
+                    }
+                    else if (betalat < b)// om beloppet man får är mindre än priset att betala   
+                    {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("fel, beloppet att betala är större än det erhållna beloppe!");
+                    Console.ResetColor();
+                    }
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("fel, fel, du kan ha angett något som inte är en siffra eller en siffra med decimal, ange en heltals siffra!");
+                    Console.ResetColor();
+                }
+
+            }
+
+            Console.WriteLine(betalat);
+            Console.WriteLine(total);
+            betalat = int.Parse(Console.ReadLine());
    
         }
     }
